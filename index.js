c298ba12a2564333c623ca09557a1356d4bb43dc
grt812 <31299429+grt812@users.noreply.github.com>
2019-04-20 14:07:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-window.onbeforeunload = function() {
-    return true;
-};
+$(function(){
+  window.onbeforeunload = function(){return true;};
+  $("#title").click(function(){
+    $("body").css("background-color","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")");
+    $(this).css("color","rgb("+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+","+Math.floor(Math.random()*256)+")");
+  });
+});
